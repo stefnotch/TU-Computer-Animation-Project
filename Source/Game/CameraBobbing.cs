79afb49d7c8a373fb778d5f78177cb3612bb0764
Stefnotch <brandmairstefan@gmail.com>
2024-06-16 14:01:54 +0200

    Init project

---
+++ Source/Game/CameraBobbing.cs
@@ -0,0 +1,54 @@
+﻿using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// Attach this script to the parent of the camera to make it bob up and down as the player moves.
+/// </summary>
+public class CameraBobbing : Script
+{
+    public float WalkSpeed = 300.0f;
+    private float _time;
+    public Camera TargetCamera;
+    private Vector3 _lastPosition;
+    /// <inheritdoc/>
+    public override void OnStart()
+    {
+        if (TargetCamera)
+        {
+            Camera.OverrideMainCamera = TargetCamera;
+        }
+    }
+
+    /// <inheritdoc/>
+    public override void OnEnable()
+    {
+        // Here you can add code that needs to be called when script is enabled (eg. register for events)
+        _lastPosition = TargetCamera.LocalPosition;
+        _time = 0.0f;
+    }
+
+    /// <inheritdoc/>
+    public override void OnDisable()
+    {
+        // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnUpdate()
+    {
+        var delta = TargetCamera.LocalPosition - _lastPosition;
+        _lastPosition = TargetCamera.LocalPosition;
+        var speed = delta.Length / Time.DeltaTime;
+
+        var speedFraction = speed / WalkSpeed;
+        _time += Time.DeltaTime * speedFraction;
+
+        var offset = new Vector3(
+            Mathf.Sin(_time * 5.0f) * 0.5f,
+            Mathf.Sin(_time * 3.0f) * 0.4f,
+            Mathf.Sin(_time * 1.0f) * 0.2f
+        ) * speedFraction;
+        Actor.LocalEulerAngles = offset;
+    }
+}
---
+++ Source/Game/FadeInMaterial.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using FlaxEngine;
+
+namespace Game;
+
+/// <summary>
+/// FadeInMaterial Script.
+/// </summary>
+public class FadeInMaterial : Script
+{
+    public float FadeInValue = 0.0f;
+    private MaterialInstance _materialInstance;
+    private MaterialParameter _hiddenParameter;
+    /// <inheritdoc/>
+    public override void OnStart()
+    {
+        _materialInstance = Actor.As<StaticModel>().CreateAndSetVirtualMaterialInstance(0);
+        _hiddenParameter = _materialInstance.GetParameter("Hidden");
+    }
+
+    /// <inheritdoc/>
+    public override void OnEnable()
+    {
+        // Here you can add code that needs to be called when script is enabled (eg. register for events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnDisable()
+    {
+        // Here you can add code that needs to be called when script is disabled (eg. unregister from events)
+    }
+
+    /// <inheritdoc/>
+    public override void OnUpdate()
+    {
+        _hiddenParameter.Value = FadeInValue;
+    }
+}
